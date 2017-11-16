6bba3808a0edc291bb5fc8c67b2891f8c5280a9c
Daniel Arrubla Funez <darrubla@uninorte.edu.co>
2017-11-16 15:08:28 -0500

    Agregar archivos de proyecto.

---
+++ MasterDetailPageNavigation/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE0028:Simplificar la inicialización de la recopilación", Justification = "<Pendiente>", Scope = "member", Target = "~M:MasterDetailPageNavigation.MasterPageCS.#ctor")]
+
